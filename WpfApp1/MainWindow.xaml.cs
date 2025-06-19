67d8c8fb6c7cb73c39d9d91272067317a815fffc
ravi <patryk1987772@wp.pl>
2025-06-19 10:38:32 +0200

    learning more about WPF media player and how stuff works... did (copied) audio player

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using ReplayParsers;
+using ReplayParsers.Classes.Beatmap.osu;
+using ReplayParsers.Decoders;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -11,9 +16,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using ReplayParsers;
-using ReplayParsers.Classes.Beatmap.osu;
-using ReplayParsers.Decoders;
 
 namespace WpfApp1
 {
@@ -25,35 +27,140 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+
             // https://stackoverflow.com/questions/46415019/how-to-draw-a-pause-button-using-wpf-path
-            canva.Width = SystemParameters.PrimaryScreenWidth;
+            //canva.Width = SystemParameters.PrimaryScreenWidth;
             canva.Height = 50;
             //Beatmap map = BeatmapDecoder.GetOsuLazerBeatmap();
-            
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += timerTick!;
-            timer.Start();
-        }
 
-        void timerTick(object sender, EventArgs e)
-        {
-            if (musicPlayer.Source != null)
-            {
-                songTimer.Text = String.Format("{0} / {1}", musicPlayer.Position.ToString(@"mm\:ss"), musicPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
-            }
-            else
-            {
-                songTimer.Text = "nothing";
-            }
-        }
-
-        void OnClick(object sender, RoutedEventArgs e)
-        {
-            background.ImageSource = new BitmapImage(new Uri($"{AppDomain.CurrentDomain.BaseDirectory}\\osu\\Background\\bg.jpg"));
+            // https://wpf-tutorial.com/audio-video/how-to-creating-a-complete-audio-video-player/
 
             musicPlayer.Source = new Uri($"{AppDomain.CurrentDomain.BaseDirectory}\\osu\\Audio\\audio.mp3");
             musicPlayer.Volume = 0.05;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+            //songTimer.Text = "";
+        }
+
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
+
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if ((mePlayer.Source != null) && (mePlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+            {
+                sliProgress.Minimum = 0;
+                sliProgress.Maximum = mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                sliProgress.Value = mePlayer.Position.TotalSeconds;
+            }
+        }
+
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mePlayer.Source = new Uri($"{AppDomain.CurrentDomain.BaseDirectory}\\osu\\Audio\\audio.mp3");
+        }
+
+        private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (mePlayer != null) && (mePlayer.Source != null);
+        }
+
+        private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mePlayer.Play();
+            mediaPlayerIsPlaying = true;
+        }
+
+        private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = mediaPlayerIsPlaying;
+        }
+
+        private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mePlayer.Pause();
+        }
+
+        private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = mediaPlayerIsPlaying;
+        }
+
+        private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mePlayer.Stop();
+            mediaPlayerIsPlaying = false;
+        }
+
+        private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            mePlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+        }
+
+        private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+        }
+
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            mePlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+        }
+
+
+
+        void timerTick(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        void StartSong(object sender, RoutedEventArgs e)
+        {
+            background.ImageSource = new BitmapImage(new Uri($"{AppDomain.CurrentDomain.BaseDirectory}\\osu\\Background\\bg.jpg"));
+
+
+            //MediaTimeline timeline = new MediaTimeline(musicPlayer.Source);
+            //musicPlayer.Clock = timeline.CreateClock(true) as MediaClock;
+            //musicPlayer.Clock!.Controller.Seek(TimeSpan.FromSeconds(0), TimeSeekOrigin.BeginTime);
+            //
+            //if (musicPlayer.Clock.CurrentProgress != null)
+            //{
+            //
+            //    
+            //}
+
+            
+
+            //button.Visibility = Visibility.Collapsed;
+            //button2.Visibility = Visibility.Visible;
+            //musicPlayer.Play();
+
+            //songTimer.Text = $"{musicPlayer.Position.ToString(@"mm\:ss")} - {musicPlayer.NaturalDuration.TimeSpan.TotalSeconds}";
+        }
+
+        void PauseSong(object sender, RoutedEventArgs e)
+        {
+            //button.Visibility = Visibility.Visible;
+            //button2.Visibility = Visibility.Collapsed;
+            //musicPlayer.Pause();
         }
     }
 }
